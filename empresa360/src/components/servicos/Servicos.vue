e2034970ab7c590370a0b6797048772265c860f8
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-10-30 11:24:18 -0300

    services - listing records

--- empresa360/src/components/servicos/Servicos.vue
+++ empresa360/src/components/servicos/Servicos.vue
@@ -1,11 +1,34 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <h5>Serviços</h5>
-</template>
-
-<script>
-export default {
+    <div>
+      <h5>Serviços</h5>
+        <hr>
+        <div class="row">
+            <div class="col" v-for="d in dados" :key="d.id">
+                <div class="card" style="width: 11rem">
+                    <img class="card-img-top" :src="`/img/${d.icone}`">
+                    <div class="card-body text-center">
+                        <p class="card-text">{{ d.servico}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  import ApiMixin from '@/mixins/ApiMixin'
+  
+  export default {
     // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Servico',
-}
-</script>
+    name: 'Servicos',
+    mixins: [ApiMixin],
+  
+    created() {
+      this.getDadosApi('http://localhost:3000/servicos')
+    }
+  
+  }
+  </script>
+  
+  
