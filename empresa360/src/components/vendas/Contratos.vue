63dbd26caaf41e327bc9d91eed1b9d883b8100ce
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-11-06 18:22:39 -0300

    contracts, listing records

--- empresa360/src/components/vendas/Contratos.vue
+++ empresa360/src/components/vendas/Contratos.vue
@@ -1,11 +1,48 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h5>Contratos</h5>
+  <div>
+    <h5>Contratos</h5>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">LEAD</th>
+          <th scope="col">SERVICO</th>
+          <th scope="col">DATA INICIO</th>
+          <th scope="col">DATA FIM</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="d in dados" :key="d.id">
+          <td>{{ d.id }}</td>
+          <td>{{ d.leadId }}</td>
+          <td>{{ d.servicoId }}</td>
+          <td>{{ d.servicoId }}</td>
+          <td>{{ d.data_inicio }}</td>
+          <td>{{ d.data_fim }}</td>
+          
+        </tr>
+      </tbody>
+    </table>
+
+
+  </div>
 </template>
 
 <script>
+import ApiMixin from '@/mixins/ApiMixin'
+
 export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Contratos',
+  mixins: [ApiMixin],
+
+  created() {
+    this.getDadosApi('http://localhost:3000/contratos')
+  }
 
 }
 </script>
+
 
